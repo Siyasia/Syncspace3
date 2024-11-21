1266caa5e1ec78096d45695234e764d215beaf6b
Siyasia <148126581+Siyasia@users.noreply.github.com>
2024-11-21 15:50:32 -0600

    Create resource.ts

---
+++ amplify/auth/resource.ts
@@ -0,0 +1,10 @@
+import { defineAuth } from '@aws-amplify/backend';
+
+/**
+ * Define and configure your auth resource
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+});
