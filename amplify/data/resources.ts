95148890856a923c7100b3448d03329be4954acd
Siyasia <148126581+Siyasia@users.noreply.github.com>
2024-11-21 15:51:32 -0600

    Create resources.ts

---
+++ amplify/data/resources.ts
@@ -0,0 +1,24 @@
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+/**
+ * SyncSpace data schema for tasks
+ */
+const schema = a.schema({
+  Task: a
+    .model({
+      content: a.string(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
+  },
+});
