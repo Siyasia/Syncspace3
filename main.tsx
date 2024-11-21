bd44879998889cd6123d5df0ee61d0422cf388ce
Siyasia <148126581+Siyasia@users.noreply.github.com>
2024-11-21 16:34:08 -0600

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,1 +1,54 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Authenticator } from '@aws-amplify/ui-react';
+import App from "./App";
+import "./index.css";
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
+
+Amplify.configure(outputs);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </React.StrictMode>
+);
+
+
+8. src/index.css
+css
+Copy code
+body {
+  margin: 0;
+  background: linear-gradient(180deg, rgb(117, 81, 194), rgb(255, 255, 255));
+  display: flex;
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+}
+
+button {
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  background-color: #1a1a1a;
+  color: white;
+  cursor: pointer;
+}
+button:hover {
+  border-color: #646cff;
+}
 
